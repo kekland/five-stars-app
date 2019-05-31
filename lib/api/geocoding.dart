eb514f30219789a76744247962b59341d418224b
kekland <kk.erzhan@gmail.com>
2019-05-31 12:43:56 +0600

    added geocoder

---
+++ lib/api/geocoding.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:five_stars/api/secret.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GeocoderApi {
+  static Future<String> getLocationName(LatLng point) async {
+    try {
+      final response = await Dio()
+          .get("https://geocode-maps.yandex.ru/1.x/", queryParameters: {
+        "apikey": YANDEX_API_KEY,
+        "format": "json",
+        "geocode": "${point.longitude},${point.latitude}",
+      });
+      List<dynamic> features = response.data['response']['GeoObjectCollection']['featureMember'];
+      List<dynamic> components = features.first['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['Components'];
+      
+      String country = components[0]['name'];
+      String city = null;
+      components.forEach((component) {
+        if(component['kind'] == 'locality') {
+          city = component['name'];
+        }
+        else if(component['kind'] == 'province') {
+          city = component['name'];
+        }
+      });
+      print(components);
+
+      return "$city, $country";
+    } catch (err) {
+      return "Ошибка";
+    }
+  }
+}
--- lib/design/select_location_page.dart
+++ lib/design/select_location_page.dart
@@ -1,8 +1,10 @@
+import 'package:five_stars/api/geocoding.dart';
 import 'package:five_stars/design/app_bar_widget.dart';
 import 'package:five_stars/design/card_widget.dart';
 import 'package:five_stars/views/two_line_information_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoder/geocoder.dart';
 
 class SelectLocationPage extends StatefulWidget {
   @override
@@ -10,6 +12,16 @@ class SelectLocationPage extends StatefulWidget {
 }
 
 class _SelectLocationPageState extends State<SelectLocationPage> {
+  String location = "Загрузка...";
+  LatLng selectedPoint;
+
+  void onSelectLocation(LatLng point) async {
+    setState(() => location = "Загрузка...");
+    selectedPoint = point;
+    final data = await GeocoderApi.getLocationName(point);
+    setState(() => location = data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,20 +41,30 @@ class _SelectLocationPageState extends State<SelectLocationPage> {
                       target: LatLng(43.238949, 76.889709),
                       zoom: 10,
                     ),
-                    onTap: (position) {},
+                    onTap: onSelectLocation,
                     myLocationButtonEnabled: true,
                     myLocationEnabled: true,
+                    markers: {
+                      Marker(
+                        markerId: MarkerId("selected-point"),
+                        infoWindow: InfoWindow(
+                          title: 'Выбранное место',
+                        ),
+                        position: selectedPoint,
+                      )
+                    },
                   ),
                   Align(
                     alignment: Alignment.bottomCenter,
                     child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 24.0),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 32.0, horizontal: 24.0),
                       child: CardWidget(
                         padding: const EdgeInsets.all(16.0),
                         body: TwoLineInformationWidget(
                           icon: Icons.pin_drop,
                           title: 'Выбранное место',
-                          value: 'Алматы, Казахстан',
+                          value: location,
                           unit: '',
                           iconColor: Colors.pink,
                         ),

febb1df2d72d3421031b89645f536ebc5ff5dee1
kekland <kk.erzhan@gmail.com>
2019-06-09 15:23:59 +0600

    Adding vehicle page

--- lib/controllers/vehicle_page_controller.dart
+++ lib/controllers/vehicle_page_controller.dart
@@ -1,39 +1,48 @@
+import 'package:five_stars/api/api.dart';
+import 'package:five_stars/api/vehicle.dart';
 import 'package:five_stars/design/future_page.dart';
-import 'package:five_stars/models/cargo_model.dart';
 import 'package:five_stars/models/vehicle_model.dart';
 import 'package:five_stars/mvc/view.dart';
-import 'package:five_stars/utils/city.dart';
-import 'package:five_stars/utils/vehicle_type.dart';
-import 'package:five_stars/utils/volume.dart';
-import 'package:five_stars/utils/weight.dart';
+import 'package:five_stars/utils/utils.dart';
 import 'package:five_stars/views/vehicle_page/vehicle_page.dart';
+import 'package:flutter/material.dart';
 
 class VehiclePageController extends Controller<VehiclePage> {
   VehiclePageController({Presenter<VehiclePage, VehiclePageController> presenter}) {
     this.presenter = presenter;
     data = null;
-    load();
   }
 
   List<Vehicle> data;
   bool loading = false;
   String error;
+  bool firstLoad = true;
 
-  Future load() async {
+  Future load(BuildContext context) async {
     loading = true;
-    error = "";
-    
+    error = null;
+    presenter.refresh();
+    try {
+      //await Future.delayed(Duration(seconds: 5));
+      data = await VehicleApi.getVehicles();
+    } catch (e) {
+      error = e.toString();
+      await Future.delayed(Duration.zero, () {
+        showErrorSnackbar(context: context, errorMessage: 'Что-то пошло не так', exception: e, showDialog: true);
+      });
+    }
+    loading = false;
+    firstLoad = false;
+    presenter.refresh();
   }
 
   FutureState getFutureState() {
-    if(loading) {
+    if (loading) {
       return FutureState.Loading;
-    }
-    else {
-      if(error != "") {
+    } else {
+      if (error != "") {
         return FutureState.Error;
-      }
-      else {
+      } else {
         return FutureState.Success;
       }
     }
--- lib/views/vehicle_page/vehicle_page.dart
+++ lib/views/vehicle_page/vehicle_page.dart
@@ -1,22 +1,39 @@
 import 'package:five_stars/controllers/vehicle_page_controller.dart';
 import 'package:five_stars/design/app_bar_widget.dart';
+import 'package:five_stars/design/circular_progress_reveal_widget.dart';
 import 'package:five_stars/design/future_page.dart';
+import 'package:five_stars/design/transparent_route.dart';
+import 'package:five_stars/models/vehicle_model.dart';
 import 'package:five_stars/mvc/view.dart';
-import 'package:five_stars/views/vehicle_page/vehicle_filter_widget.dart';
+import 'package:five_stars/utils/utils.dart';
 import 'package:five_stars/views/vehicle_page/vehicle_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
 class VehiclePage extends StatefulWidget {
   const VehiclePage({Key key}) : super(key: key);
-
   @override
-  VehiclePageState createState() => VehiclePageState();
+  _VehiclePageState createState() => _VehiclePageState();
 }
 
-class VehiclePageState extends Presenter<VehiclePage, VehiclePageController> {
+class _VehiclePageState extends Presenter<VehiclePage, VehiclePageController> {
   @override
   void initController() {
     controller = VehiclePageController(presenter: this);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller.load(context);
+  }
+
+  void addVehicle(BuildContext context) {
+    Navigator.of(context).push(TransparentRoute(
+      builder: (_) {
+        //return CargoAlterPage(mainContext: context, mode: AlterMode.add);
+      },
+    ));
   }
 
   @override
@@ -29,6 +46,31 @@ class VehiclePageState extends Presenter<VehiclePage, VehiclePageController> {
             children: <Widget>[
               AppBarWidget(
                 title: Text('Свободный транспорт'),
+                action: IconButton(
+                    icon: Icon(Icons.add),
+                    color: Colors.purple,
+                    onPressed: () => addVehicle(context)),
+              ),
+              Expanded(
+                child: Stack(
+                  children: <Widget>[
+                    if (controller.firstLoad && controller.data == null)
+                      Center(
+                        child: CircularProgressRevealWidget(color: Colors.purple),
+                      ),
+                    LiquidPullToRefresh(
+                      color: Colors.purple,
+                      springAnimationDurationInMilliseconds: 500,
+                      child: buildDataPage<Vehicle>(
+                        accentColor: Colors.purple,
+                        builder: (context, item) => VehicleWidget(data: item),
+                        data: controller.data,
+                        error: controller.error,
+                      ),
+                      onRefresh: () async => await controller.load(context),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
